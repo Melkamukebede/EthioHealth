2b589a593f9690a500f0807b61858a5caf004ead
Melkamu Kebede <melkamukebede11@gmail.com>
2026-08-04 23:28:40 +0300

    Add routes for single herb and interaction checks

--- backend/src/routes/herbs.js
+++ backend/src/routes/herbs.js
@@ -2,8 +2,16 @@ const express = require('express');
 const router = express.Router();
 const herbController = require('../controllers/herbController');
 
+// Get all herbs
 router.get('/', herbController.getAllHerbs);
+
+// Search herbs
 router.get('/search', herbController.searchHerbs);
-router.get('/:id', herbController.getHerbById);
+
+// Get single herb
+router.get('/:id', herbController.getHerb);
+
+// Check herb-drug interactions
+router.post('/check-interactions', herbController.checkInteractions);
 
 module.exports = router;
