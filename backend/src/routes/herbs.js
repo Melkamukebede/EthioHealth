d708bb4a5216e6514737ded631c2d4a2e4360ddc
Melkamu Kebede <melkamukebede11@gmail.com>
2026-08-04 20:22:11 +0300

    Refactor herb routes to simplify API endpoints

--- backend/src/routes/herbs.js
+++ backend/src/routes/herbs.js
@@ -2,16 +2,8 @@ const express = require('express');
 const router = express.Router();
 const herbController = require('../controllers/herbController');
 
-// Get all herbs
 router.get('/', herbController.getAllHerbs);
-
-// Search herbs
 router.get('/search', herbController.searchHerbs);
-
-// Get single herb
-router.get('/:id', herbController.getHerb);
-
-// Check herb-drug interactions
-router.post('/check-interactions', herbController.checkInteractions);
+router.get('/:id', herbController.getHerbById);
 
 module.exports = router;
