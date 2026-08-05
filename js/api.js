4dd72ccef5c7f1cdaa846d091da7897db67429f9
Melkamu Kebede <melkamukebede11@gmail.com>
2026-08-05 06:52:58 +0300

    Fix formatting issue in api.js

--- js/api.js
+++ js/api.js
@@ -6,7 +6,7 @@ const API = (function() {
     'use strict';
     
     // Change this to your Render.com URL after deployment
-    const BASE_URL = 'https://ethiohealth-api.onrender.com/api/v1';
+const BASE_URL = 'https://ethiohealth-api.onrender.com/api/v1';
 
     const TIMEOUT = 10000;
     
